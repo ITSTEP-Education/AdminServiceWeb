407bad7a27108c890adb3e185db756194b92d993
qwertyOko <Koval_wl91@student.itstep.org>
2024-09-28 10:26:27 +0300

    main page
    -create container for main
    -create a form Authentication
    -create title

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Title from '../Title/Title';
+import './Body.css'
+import Authentication from '../Authentication/Authentication';
+
+const  Body = () => {
+
+    return (
+        <>
+        <div className='mainPage'>
+
+        <Title/>
+        <Authentication/>
+        </div>
+        
+        
+
+
+
+
+
+        
+        
+        </>
+    )
+}
+
+export default Body
